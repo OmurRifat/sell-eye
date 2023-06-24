f818a710f9a10ec134e43967008ccc475a79cca9
OmurRifat <jabedomur.ece.ku@gmail.com>
2023-06-24 20:35:19 +0600

    pagination added

--- src/Pages/AllOrders.jsx
+++ src/Pages/AllOrders.jsx
@@ -3,9 +3,41 @@ import React, { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../context/AuthProvider'
 
 export default function AllOrders() {
-    const { orders, setOrders } = useContext(AuthContext)
+    // const { orders, setOrders } = useContext(AuthContext)
+    const [page, setPage] = useState(0)
+    const [count, setCount] = useState(0)
+
+    const [orders, setOrders] = useState([])
+    useEffect(() => {
+        fetch('/ordersInfo.json')
+            .then(res => res.json())
+            .then(data => {
+                setCount((data.length / 5));
+                switch (page) {
+                    case 0:
+                        setOrders(data.slice(0, 5))
+                        break;
+                    case 1:
+                        setOrders(data.slice(5, 10))
+                        break;
+                    case 2:
+                        setOrders(data.slice(10, 15))
+                        break;
+                    case 3:
+                        setOrders(data.slice(15, 20))
+                        break;
+                    case 4:
+                        setOrders(data.slice(20, 25))
+                        break;
+                    default:
+                        setOrders(data.slice(0, 5))
+                        break;
+                }
+            })
+        console.log(page);
+    }, [page])
     return (
-        <div>
+        <div className=' '>
             {/* create a table */ }
             <table className="table w-full bg-tertiary bg-opacity-30">
                 <thead>
@@ -43,6 +75,17 @@ export default function AllOrders() {
                     }
                 </tbody>
             </table>
+            <div className="text-center mt-6">
+                {
+                    [...Array(count).keys()].map(number => <button
+                        key={ number }
+                        className={ page == number ? ' bg-red-500 p-3 rounded-lg font-bold focus:outline-none' : ' bg-gray-400 text-black p-3 mx-1 rounded-lg font-bold' }
+                        onClick={ () => setPage(number) }
+                    >
+                        { number + 1 }
+                    </button>)
+                }
+            </div>
         </div>
     )
 }
